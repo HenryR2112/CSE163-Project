e0d6bdbbd1310202292773cc9fa804dbedd05261
HenryR2112 <henry.ramstad@gmail.com>
2023-05-28 14:21:32 -0700

    filter fixed

---
+++ filter.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df_fremont = pd.read_csv('Raw Data/Burke_Gilman_Trail_north_of_NE_70th_St_Bicycle_and_Pedestrian_Counter.csv')
+df_burke = pd.read_csv('Raw Data/Elliott_Bay_Trail_in_Myrtle_Edwards_Park_Bicycle_and_Pedestrian_Counter.csv')
+df_elliott = pd.read_csv('Raw Data/Fremont_Bridge_Bicycle_Counter.csv')
+df_ballard = pd.read_csv('Raw Data/NW_58th_St_Greenway_at_22nd_Ave_NW_Bicycle_Counter.csv')
+
+
