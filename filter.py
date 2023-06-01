502d7c087a8ce86f61e18a72f4f1143d8ffdfbf0
Annika Halvorson <annikahalvorson03@gmail.com>
2023-05-31 18:07:13 -0700

    test

--- filter.py
+++ filter.py
@@ -9,9 +9,9 @@ redefining columns to create 4 identical columns of data
 '''
 import pandas as pd
 
-main():
-	df_burke = pd.read_csv('Raw Data/Burke_Gilman_Trail_north_of_NE_70th_St_Bicycle_and_Pedestrian_Counter.csv', index_col='Date', parse_dates=True)
-	df_elliott = pd.read_csv('Raw Data/Elliott_Bay_Trail_in_Myrtle_Edwards_Park_Bicycle_and_Pedestrian_Counter.csv', index_col='Date', parse_dates=True)
+def main():
+    df_burke = pd.read_csv('Raw Data/Burke_Gilman_Trail_north_of_NE_70th_St_Bicycle_and_Pedestrian_Counter.csv', index_col='Date', parse_dates=True)
+    df_elliott = pd.read_csv('Raw Data/Elliott_Bay_Trail_in_Myrtle_Edwards_Park_Bicycle_and_Pedestrian_Counter.csv', index_col='Date', parse_dates=True)
 	df_fremont = pd.read_csv('Raw Data/Fremont_Bridge_Bicycle_Counter.csv', index_col='Date', parse_dates=True)
 	df_ballard = pd.read_csv('Raw Data/NW_58th_St_Greenway_at_22nd_Ave_NW_Bicycle_Counter.csv', index_col='Date', parse_dates=True)
 
